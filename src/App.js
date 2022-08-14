973a5f042c566db6bf0b3d75a0cbb2cc50277283
Daniel Gonzalez <danielgnzch@gmail.com>
2022-08-14 17:16:07 +0200

    Iterarion 5 done

--- src/App.js
+++ src/App.js
@@ -3,14 +3,29 @@ import './App.css';
 import foods from './foods.json';
 import FoodBox from './components/FoodBox';
 import AddFoodForm from './components/AddFoodForm';
+import Search from './components/Search';
 
 function App() {
   const [foodsList, setFoods] = useState(foods);
+  const [foodsListWithAdded, setAddedList] = useState(foods);
 
   const handleNewFood = (food) => {
     const updatedFoods = [...foodsList];
     updatedFoods.unshift(food);
     setFoods(updatedFoods);
+    /*Added state for not lost the added item whem reset the search bar list */
+    setAddedList(updatedFoods);
+  };
+
+  const handleSearch = (searchValue) => {
+    if (searchValue === '') {
+      setFoods(foodsListWithAdded);
+    } else {
+      const filtered = foodsListWithAdded.filter((elem) =>
+        elem.name.toLowerCase().includes(searchValue.toLowerCase())
+      );
+      setFoods(filtered);
+    }
   };
 
   return (
@@ -19,6 +34,10 @@ function App() {
         <h1 className="separator">Add Food Entry</h1>
       </div>
       <AddFoodForm newFood={handleNewFood} />
+      <div className="container">
+        <h1 className="separator">Search</h1>
+      </div>
+      <Search onSearch={handleSearch} />
       <div className="container">
         <h1 className="separator">Food List</h1>
       </div>
---
+++ src/components/Search.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Input } from 'antd';
+
+export default function Search({ onSearch }) {
+  return (
+    <div className="container">
+      <label>Search</label>
+      <Input
+        type="text"
+        placeholder="Enter search query"
+        onChange={(e) => onSearch(e.target.value)}
+      />
+    </div>
+  );
+}
